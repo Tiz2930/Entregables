c1e95a1dbf8dc211d6205de3c9ccd11621c2cb08
Tiz2930 <168146573+Tiz2930@users.noreply.github.com>
2024-09-30 17:26:01 -0300

    Correcciones

--- fetch.js
+++ fetch.js
@@ -1,21 +1,15 @@
-fetch("https://hp-api.onrender.com/api/characters")
-    .then(resp => resp.json())
+let contenedor = document.getElementById("contenedor");
+
+
+fetch("https://fakestoreapi.com/products")
+    //    .then(resp => console.log(resp))
+    .then(resp => resp.json()) //convierte en json
+
     .then(data => {
-        const firstTenCharacters = data.slice(0, 10); // Limita a los primeros 10 personajes
-        firstTenCharacters.forEach(prod => {
-            // Crear un nuevo div para cada personaje
-            const card = document.createElement("div");
-            card.classList.add("card");
-
-            // Añadir contenido a la tarjeta
-            card.innerHTML = `<h2> Nombre: ${prod.name}</h2>
-                              <h4> Casa: ${prod.house}</h4>`;
-
-            // Añadir la tarjeta al contenedor
-            contenedor.appendChild(card);
-        });
-    })
-
-//promesas()
-// .then(resp => console.log(resp)) //SI ES EXITOSA
-// .catch(err => console.log(err)) //SI HAY ERROR LO CAPTURAMOS
+        data.forEach(prod => {
+            contenedor.innerHTML += `<h2> Nombre:  ${prod.title} </h2>
+                                     <h4> Precio:  ${prod.price} </h4>`
+        })                           
+        })
+            // .then(data => console.log(data)) //muestra la data
+            .catch(error => console.log(error)) //muestra el error
